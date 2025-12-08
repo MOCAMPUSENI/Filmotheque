5fd8453c4fe282f8263a4dce610e456055949916
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-08 12:28:13 +0100

    [edit] - init jdbctemplate and the constructor in AvisRepositoryImpl and MembresRepositoryImpl

--- src/main/java/fr/eni/tp/filmoteque/dal/AvisRepositoryImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/dal/AvisRepositoryImpl.java
@@ -1,10 +1,17 @@
 package fr.eni.tp.filmoteque.dal;
 
 import fr.eni.tp.filmoteque.bo.Avis;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 public class AvisRepositoryImpl implements AvisRepository {
+    private JdbcTemplate jdbcTemplate;
+    
+    public AvisRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    
     @Override
     public List<Avis> findAllAvis() {
         return List.of();
--- src/main/java/fr/eni/tp/filmoteque/dal/MembresRepositoryImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/dal/MembresRepositoryImpl.java
@@ -1,10 +1,16 @@
 package fr.eni.tp.filmoteque.dal;
 
 import fr.eni.tp.filmoteque.bo.Membre;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 public class MembresRepositoryImpl implements MembresRepository {
+    private JdbcTemplate jdbcTemplate;
+    
+    public MembresRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
     @Override
     public List<Membre> findAllMembres() {
         return List.of();
