c4fc39e26592f3d7d29e57e65e53cb2ebccd610e
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-08 12:25:03 +0100

    [edit] - add method in MembresRepository.java interface

--- src/main/java/fr/eni/tp/filmoteque/dal/MembresRepository.java
+++ src/main/java/fr/eni/tp/filmoteque/dal/MembresRepository.java
@@ -1,4 +1,10 @@
 package fr.eni.tp.filmoteque.dal;
 
+import fr.eni.tp.filmoteque.bo.Membre;
+
+import java.util.List;
+
 public interface MembresRepository {
+    List<Membre> findAllMembres();
+    Membre findMembreById(int id);
 }
