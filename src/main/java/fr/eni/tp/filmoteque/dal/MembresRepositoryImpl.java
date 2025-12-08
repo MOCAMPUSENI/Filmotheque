49e48a5d05c9020015ecab4d5585b3e150b7f0e5
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-08 12:26:48 +0100

    [edit] - add override method in MembresRepository.java

--- src/main/java/fr/eni/tp/filmoteque/dal/MembresRepositoryImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/dal/MembresRepositoryImpl.java
@@ -1,4 +1,17 @@
 package fr.eni.tp.filmoteque.dal;
 
+import fr.eni.tp.filmoteque.bo.Membre;
+
+import java.util.List;
+
 public class MembresRepositoryImpl implements MembresRepository {
+    @Override
+    public List<Membre> findAllMembres() {
+        return List.of();
+    }
+    
+    @Override
+    public Membre findMembreById(int id) {
+        return null;
+    }
 }
