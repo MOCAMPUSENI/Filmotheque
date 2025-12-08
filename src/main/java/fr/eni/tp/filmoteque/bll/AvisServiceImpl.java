8a76c08faefe3ca3ed18e8d77f975eb3414601ce
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-08 13:35:37 +0100

    [add] - add override methods from AvisServiceImpl and MembreService

--- src/main/java/fr/eni/tp/filmoteque/bll/AvisServiceImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/bll/AvisServiceImpl.java
@@ -1,4 +1,30 @@
 package fr.eni.tp.filmoteque.bll;
 
+import fr.eni.tp.filmoteque.bo.Avis;
+import fr.eni.tp.filmoteque.dal.AvisRepositoryImpl;
+
+import java.util.List;
+
 public class AvisServiceImpl implements AvisService {
+    AvisRepositoryImpl avisRepositoryImpl;
+    
+    public AvisServiceImpl(AvisRepositoryImpl avisRepositoryImpl) {
+        this.avisRepositoryImpl = avisRepositoryImpl;
+    }
+    
+    public List<Avis> findAllAvis() {
+        return avisRepositoryImpl.findAllAvis();
+    }
+
+    public Avis findAvisById(int id) {
+       return avisRepositoryImpl.findAvisById(id);
+    }
+    
+    public List<Avis> findAllAvisByMembreId(int membreId) {
+        return avisRepositoryImpl.findAllAvisByMembreId(membreId);
+    }
+    
+    public List<Avis> findAllAvisByFilmId(int filmId) {
+        return  avisRepositoryImpl.findAllAvisByFilmId(filmId);
+    }
 }
--- src/main/java/fr/eni/tp/filmoteque/bll/MembreServiceImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/bll/MembreServiceImpl.java
@@ -1,4 +1,22 @@
 package fr.eni.tp.filmoteque.bll;
 
+import fr.eni.tp.filmoteque.bo.Membre;
+import fr.eni.tp.filmoteque.dal.MembresRepositoryImpl;
+
+import java.util.List;
+
 public class MembreServiceImpl implements MembreService {
+    MembresRepositoryImpl membresRepositoryImpl;
+    
+    public MembreServiceImpl(MembresRepositoryImpl membresRepositoryImpl) {
+        this.membresRepositoryImpl = membresRepositoryImpl;
+    }
+    
+    public List<Membre>  findAllMembres() {
+        return membresRepositoryImpl.findAllMembres();
+    }
+    
+    public Membre findMembreById(int id) {
+        return membresRepositoryImpl.findMembreById(id);
+    }
 }
