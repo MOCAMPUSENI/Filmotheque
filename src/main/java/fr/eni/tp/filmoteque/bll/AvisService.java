2f30371ba0fcb5819062e45550aa3ab832838119
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-08 13:34:59 +0100

    [add] - add method in AvisService & MembreService interface

--- src/main/java/fr/eni/tp/filmoteque/bll/AvisService.java
+++ src/main/java/fr/eni/tp/filmoteque/bll/AvisService.java
@@ -1,4 +1,12 @@
 package fr.eni.tp.filmoteque.bll;
 
+import fr.eni.tp.filmoteque.bo.Avis;
+
+import java.util.List;
+
 public interface AvisService {
+    List<Avis> findAllAvis();
+    Avis findAvisById(int id);
+    List<Avis> findAllAvisByMembreId(int membreId);
+    List<Avis> findAllAvisByFilmId(int filmId);
 }
--- src/main/java/fr/eni/tp/filmoteque/bll/MembreService.java
+++ src/main/java/fr/eni/tp/filmoteque/bll/MembreService.java
@@ -1,4 +1,10 @@
 package fr.eni.tp.filmoteque.bll;
 
+import fr.eni.tp.filmoteque.bo.Membre;
+
+import java.util.List;
+
 public interface MembreService {
+    List<Membre> findAllMembres();
+    Membre findMembreById(int id);
 }
