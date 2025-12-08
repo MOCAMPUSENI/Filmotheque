ded8f7bb91944a98e181613adf1b57c0afcc79e2
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-08 12:38:09 +0100

    [edit] - delete all @Override and add th rowmapper

--- src/main/java/fr/eni/tp/filmoteque/dal/AvisRepositoryImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/dal/AvisRepositoryImpl.java
@@ -1,8 +1,12 @@
 package fr.eni.tp.filmoteque.dal;
 
 import fr.eni.tp.filmoteque.bo.Avis;
+import fr.eni.tp.filmoteque.bo.Membre;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class AvisRepositoryImpl implements AvisRepository {
@@ -12,23 +16,30 @@ public class AvisRepositoryImpl implements AvisRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
     
-    @Override
     public List<Avis> findAllAvis() {
         return List.of();
     }
     
-    @Override
     public Avis findAvisById(int id) {
         return null;
     }
     
-    @Override
     public List<Avis> findAllAvisByMembreId(int membreId) {
         return List.of();
     }
     
-    @Override
     public List<Avis> findAllAvisByFilmId(int filmId) {
         return List.of();
+    }
+    
+    static class AvisRowMapper implements RowMapper<Avis> {
+        public Avis mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Avis avis = new Avis();
+            avis.setId(rs.getInt("id"));
+            avis.setNote(rs.getInt("note"));
+            avis.setCommentaire(rs.getString("commentaire"));
+            avis.setMembreId(rs.getInt("membreId"));
+            return avis;
+        }
     }
 }
