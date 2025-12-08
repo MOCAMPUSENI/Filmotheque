e33f53e38764b446ba9e67db41122c7dd077d8fa
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-08 12:27:01 +0100

    [edit] - add override method in AvisRepositoryImpl.java

--- src/main/java/fr/eni/tp/filmoteque/dal/AvisRepositoryImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/dal/AvisRepositoryImpl.java
@@ -1,4 +1,27 @@
 package fr.eni.tp.filmoteque.dal;
 
+import fr.eni.tp.filmoteque.bo.Avis;
+
+import java.util.List;
+
 public class AvisRepositoryImpl implements AvisRepository {
+    @Override
+    public List<Avis> findAllAvis() {
+        return List.of();
+    }
+    
+    @Override
+    public Avis findAvisById(int id) {
+        return null;
+    }
+    
+    @Override
+    public List<Avis> findAllAvisByMembreId(int membreId) {
+        return List.of();
+    }
+    
+    @Override
+    public List<Avis> findAllAvisByFilmId(int filmId) {
+        return List.of();
+    }
 }
