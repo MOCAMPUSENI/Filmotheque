7917f67133db122308c7a76589a836ee1b72120d
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-09 11:20:34 +0100

    [edit] -add the GenreService part - GenreService.java, GenreServiceImpl.java

--- src/main/java/fr/eni/tp/filmoteque/bll/GenreService.java
+++ src/main/java/fr/eni/tp/filmoteque/bll/GenreService.java
@@ -5,5 +5,8 @@ import fr.eni.tp.filmoteque.bo.Genre;
 import java.util.List;
 
 public interface GenreService {
-
+    List<Genre> findAllGenres();
+    Genre findGenreById(int id);
+    void addGenre(String libelle);
+    void updateGenre(String newLibelle, int id);
 }
--- src/main/java/fr/eni/tp/filmoteque/bll/GenreServiceImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/bll/GenreServiceImpl.java
@@ -1,25 +1,39 @@
 package fr.eni.tp.filmoteque.bll;
 
 import fr.eni.tp.filmoteque.bo.Genre;
+import fr.eni.tp.filmoteque.dal.GenreRepository;
 import fr.eni.tp.filmoteque.dal.GenreRepositoryImpl;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//@Service
+@Service
 public class GenreServiceImpl implements GenreService {
-    // GenreRepositoryImpl genreRepository;
-    //
-    // public GenreServiceImpl(GenreRepositoryImpl genreRepository) {
-    //     this.genreRepository = genreRepository;
-    // }
-    //
-    // public List<Genre> findAllGenres() {
-    //     return genreRepository.findAllGenres();
-    // }
-    //
-    // public Genre findGenreById(int id) {
-    //     return genreRepository.findGenreById(id);
-    // }
+    // ? GenreService
+    GenreRepository genreRepository;
+    
+    public GenreServiceImpl(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+    
+    @Cacheable("genres")
+    public List<Genre> findAllGenres() {
+        return genreRepository.findAllGenres();
+    }
+    
+    public Genre findGenreById(int id) {
+        return genreRepository.findGenreById(id);
+    }
+    
+    @CacheEvict(value = "genres", allEntries = true)
+    public void addGenre(String libelle) {
+        genreRepository.addGenre(libelle);
+    }
+    
+    @CacheEvict(value = "genres", allEntries = true)
+    public void updateGenre(String newLibelle, int id) {
+        genreRepository.updateGenre(newLibelle, id);
+    }
 }
