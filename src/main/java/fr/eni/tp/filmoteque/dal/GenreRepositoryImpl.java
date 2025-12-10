cf97833253e41f7c651fbc531880acca0a9e9ffb
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-10 09:01:35 +0100

    [edit] - add view and stored procedure - GenreRepositoryImpl.java

--- src/main/java/fr/eni/tp/filmoteque/dal/GenreRepositoryImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/dal/GenreRepositoryImpl.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -20,30 +21,30 @@ public class GenreRepositoryImpl implements GenreRepository {
     }
     
     public List<Genre> findAllGenres() {
-        String sql = "select id, libelle from genres";
+        String sql = "select * from dbo.findAllGenres";
         return jdbcTemplate.query(sql, new GenreRowMapper());
     }
     
     public Genre findGenreById(int id) {
-        String sql = "select id, libelle from genres where id = ?";
+        String sql = "select * from dbo.findAllGenres where id = ?";
         return jdbcTemplate.queryForObject(sql, new GenreRowMapper(), id);
     }
     
     public void addGenre(String libelle) {
-        String sql = "insert into genres (libelle) values (?)";
-        jdbcTemplate.update(sql, libelle);
+        jdbcTemplate.update(connection -> {
+            CallableStatement cs = connection.prepareCall("{call create_genre(?)}");
+            cs.setString(1, libelle);
+            return cs;
+        });
     }
     
     public void updateGenre(String newLibelle, int id) {
-        String sql = "update genres set libelle = ? where id = ?";
-        PreparedStatementSetter pss = new PreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps) throws SQLException {
-                ps.setString(1, newLibelle);
-                ps.setInt(2, id);
-            }
-        };
-        jdbcTemplate.update(sql, pss);
+        jdbcTemplate.update(connection -> {
+            CallableStatement cs = connection.prepareCall("{call update_genre(?,?)}");
+            cs.setString(1, newLibelle);
+            cs.setInt(2, id);
+            return cs;
+        });
     }
     
     static class GenreRowMapper implements RowMapper<Genre> {
