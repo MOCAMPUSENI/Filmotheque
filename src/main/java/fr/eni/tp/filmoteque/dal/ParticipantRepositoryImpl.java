16da90f854cd826a634a7add45f4725c62644ed0
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-10 09:01:54 +0100

    [edit] - add view and stored procedure - ParticipantRepositoryImpl.java

--- src/main/java/fr/eni/tp/filmoteque/dal/ParticipantRepositoryImpl.java
+++ src/main/java/fr/eni/tp/filmoteque/dal/ParticipantRepositoryImpl.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -19,33 +20,41 @@ public class ParticipantRepositoryImpl implements ParticipantRepository {
     }
     
     public List<Participant> getAllParticipants() {
-        String sql = "select * from participants order by id";
+        String sql = "select * from dbo.getAllParticipants";
         return jdbcTemplate.query(sql, new ParticipantRowMapper());
     }
     
     public Participant getParticipantById (int id) {
-        String sql =  "select * from participants where id = ?";
+        String sql =  "select * from dbo.getAllParticipants where id = ?";
         return jdbcTemplate.queryForObject(sql, new ParticipantRowMapper(), id);
     }
     
     public List<Participant> getAllParticipantsByFilmId (int filmId) {
-        String sql =  "SELECT p.* FROM acteurs a INNER JOIN participants p ON p.id = a.participantId WHERE a.filmId = ?";
+        String sql =  "SELECT * FROM dbo.getAllParticipantByFilmId WHERE filmId = ?";
         return jdbcTemplate.query(sql, new ParticipantRowMapper(), filmId);
     }
     
     public List<FilmDTO> getAllFilmsByParticipant (int id) {
-        return null;
-        
+        String sql =  "SELECT * FROM dbo.getAllFilmsByParticipant WHERE participantId = ?";
+        return jdbcTemplate.query(sql, new FilmRowMapper(), id);
     }
     
     public void addParticipant(Participant participant) {
-        String sql = "INSERT INTO participants VALUES (?, ?)";
-        jdbcTemplate.update(sql, participant.getPrenom(), participant.getNom());
+        jdbcTemplate.update(connection -> {
+            CallableStatement cs = connection.prepareCall("{call create_participant(?, ?)}");
+            cs.setString(1, participant.getPrenom());
+            cs.setString(2, participant.getNom());
+            return cs;
+        });
     }
     
     public void addParticipantToFilm(int participantId, int filmId) {
-        String sql = "INSERT INTO acteurs VALUES (?, ?)";
-        jdbcTemplate.update(sql, filmId, participantId);
+        jdbcTemplate.update(connection -> {
+            CallableStatement cs = connection.prepareCall("{call create_acteur(?, ?)}");
+            cs.setInt(1, filmId);
+            cs.setInt(2, participantId);
+            return cs;
+        });
     }
     
     static class ParticipantRowMapper implements RowMapper<Participant> {
@@ -55,6 +64,20 @@ public class ParticipantRepositoryImpl implements ParticipantRepository {
             participant.setPrenom(rs.getString("prenom"));
             participant.setNom(rs.getString("nom"));
             return participant;
+        }
+    }
+    
+    static class FilmRowMapper implements RowMapper<FilmDTO> {
+        public FilmDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+            FilmDTO film = new FilmDTO();
+            film.setId(rs.getInt("id"));
+            film.setTitre(rs.getString("titre"));
+            film.setAnne(rs.getInt("annee"));
+            film.setDuree(rs.getInt("duree"));
+            film.setSynopsis(rs.getString("synopsis"));
+            film.setGenreId(rs.getInt("genreId"));
+            film.setRealisateurId(rs.getInt("realisateurId"));
+            return film;
         }
     }
 }
