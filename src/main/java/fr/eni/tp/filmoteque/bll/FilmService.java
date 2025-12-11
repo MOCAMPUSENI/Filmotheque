a2a529b768fd76833ba8831eecf497907046a3eb
Morgan <morgan.orain2025@campus-eni.fr>
2025-12-11 16:47:05 +0100

    [edit] - clean code - FilmService.java

--- src/main/java/fr/eni/tp/filmoteque/bll/FilmService.java
+++ src/main/java/fr/eni/tp/filmoteque/bll/FilmService.java
@@ -1,8 +1,5 @@
 package fr.eni.tp.filmoteque.bll;
 
-import fr.eni.tp.filmoteque.bo.Film;
-import fr.eni.tp.filmoteque.bo.Genre;
-import fr.eni.tp.filmoteque.bo.Participant;
 import fr.eni.tp.filmoteque.dto.FilmDTO;
 
 import java.util.List;
@@ -12,10 +9,4 @@ public interface FilmService {
     List<FilmDTO> findAllFilmsByRealisateur(int id);
     FilmDTO consulterFilmParId(int id);
     int creerFilm(FilmDTO film);
-    
-    // GenreService
-    // List<Genre> findAllGenres();
-    // Genre findGenreById(int id);
-    // void addGenre(String libelle);
-    // void updateGenre(String newLibelle, int id);
 }
